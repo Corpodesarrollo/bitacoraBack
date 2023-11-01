63287934bc43994b79cbde521b8d057a64c7f50d
mmanjarres <mmanjarres@mincit.gov.co>
2023-11-01 16:01:43 -0500

    error en duplicidad de repositorio TipoLogRepository.java, se implementa base de datos de prueba 20.237.244.76:1521

--- src/main/java/linktic/lookfeel/security/repositories/TipoLogRepository.java
+++
@@ -1,9 +0,0 @@
-package linktic.lookfeel.security.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import linktic.lookfeel.model.TipoLog;
-
-public interface TipoLogRepository extends JpaRepository<TipoLog,Long>{
-
-}
